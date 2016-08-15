95b8d67e3f592b63660db5e3b87e87d9231f0635
qwezhb <1244275211@qq.com>
2016-08-15 12:09:15 +0800

    zhb

---
+++ js/js.js
@@ -0,0 +1,39 @@
+function fontAuto(width){
+	var s=document.documentElement.clientWidth;
+	if(s>=width)
+	{
+		document.documentElement.style.fontSize="625%";
+		
+	}
+	else
+	{
+		document.documentElement.style.fontSize=(625*s/width)+"%";
+		
+	}
+	
+	}
+	fontAuto(750);
+	window.onresize=function(){
+		fontAuto(750);
+	
+}
+
+
+//function fontAuto(width){//width表示效果图的宽度
+//	var screenWidth=document.documentElement.clientWidth;//屏幕的宽度
+//	//document.documentElement表示html标签
+//	if(screenWidth>=width)//当设备的宽度大于等于效果图的宽度
+//	{
+//		document.documentElement.style.fontSize="625%";
+//	}
+//	else{//当设备的宽度小于效果图的宽度
+//		document.documentElement.style.fontSize=(625*screenWidth/width)+"%";
+//	}
+//	
+//}
+//
+//fontAuto(750);//网页加载的时候触发
+//
+//window.onresize=function(){//当网页宽度发生变化时触发函数
+//	fontAuto(750);
+//}
